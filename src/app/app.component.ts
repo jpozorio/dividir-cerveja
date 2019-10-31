1b71d07f8aece77018f9c42cabf648d063affd58
João Paulo Ozório da Silva <jpozorio@gmail.com>
2019-10-30 23:01:22 -0300

    Melhorias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,66 +27,14 @@ export class AppComponent {
     const last: Drinker = this._tableService.drinkers[qtd - 1];
     if (last.name) {
       this.addNewDrinker();
+      this._tableService.startInChanged();
     }
-  }
-
-  getTotalAmmount(): number {
-    let totalAmmount = 0;
-    let qtd = this._tableService.drinkers.length;
-    let last: Drinker;
-    for (let d of this._tableService.drinkers) {
-      let number = qtd--;
-      if (!last || last.start_in != d.start_in) {
-        totalAmmount += (this._tableService.beer_ammount - totalAmmount - d.start_in) / number;
-      }
-      last = d;
-    }
-    return totalAmmount;
-  }
-
-  startInChanged() {
-    const total = this.getTotalAmmount();
-    const beerAmmount = this._tableService.beer_ammount;
-    if (total > beerAmmount || beerAmmount == 0 || total < 0) {
-      return;
-    }
-    let qtdeToSplit = [];
-    let drinkers = this._tableService.drinkers.filter(d => d.name != '');
-    for (let i = 1; i <= beerAmmount; i++) {
-      let qtde = 0;
-      for (const drinkerCalculating of drinkers) {
-        drinkerCalculating.ammout_drinked = 0;
-        const end = this.fim(drinkerCalculating, beerAmmount);
-        if (i >= drinkerCalculating.start_in && i <= end) {
-          qtde++;
-        }
-      }
-      qtdeToSplit[i] = qtde;
-    }
-
-    for (let index = 1; index < qtdeToSplit.length; index++) {
-      const qtde = qtdeToSplit[index];
-      for (const drinkerCalculating of drinkers) {
-        const end = this.fim(drinkerCalculating, beerAmmount);
-        if (index >= drinkerCalculating.start_in && index <= end) {
-          drinkerCalculating.ammout_drinked += 1 / qtde;
-        }
-      }
-    }
-
-    for (const drinkerCalculating of drinkers) {
-      drinkerCalculating.should_pay = (drinkerCalculating.ammout_drinked * this._tableService.beer_unit_value) * (this._tableService.includeTip ? 1.1 : 1);
-    }
-  }
-
-  fim(d1: Drinker, beerAmmount: number) {
-    return d1.end_in ? d1.end_in : beerAmmount;
   }
 
   deleteDrinker(idx: number) {
     this._tableService.drinkers.splice(idx, 1);
     if (this._tableService.drinkers.length > 0) {
-      this.startInChanged();
+      this._tableService.startInChanged();
     }
   }
 
@@ -96,6 +44,6 @@ export class AppComponent {
     } else {
       drinker.end_in = null;
     }
-    this.startInChanged();
+    this._tableService.startInChanged();
   }
 }
--- src/app/table.service.ts
+++ src/app/table.service.ts
@@ -38,9 +38,63 @@ export class TableService {
 
   addBeer() {
     this.beer_ammount++;
+    this.startInChanged();
   }
 
   rmBeer() {
-    this.beer_ammount-- ;
+    if (this.beer_ammount > 0) {
+      this.beer_ammount--;
+      this.startInChanged();
+    }
   }
+
+  getTotalAmmount(): number {
+    let totalAmmount = 0;
+    for (let d of this.drinkers) {
+      if (d.start_in > totalAmmount) {
+        totalAmmount = d.start_in;
+      }
+    }
+    return totalAmmount;
+  }
+
+  startInChanged() {
+    const total = this.getTotalAmmount();
+    const beerAmmount = this.beer_ammount;
+    if (total > beerAmmount || beerAmmount == 0 || total < 0) {
+      return;
+    }
+    let qtdeToSplit = [];
+    let drinkers = this.drinkers.filter(d => d.name != '');
+    for (let i = 1; i <= beerAmmount; i++) {
+      let qtde = 0;
+      for (const drinkerCalculating of drinkers) {
+        drinkerCalculating.ammout_drinked = 0;
+        const end = this.fim(drinkerCalculating, beerAmmount);
+        if (i >= drinkerCalculating.start_in && i <= end) {
+          qtde++;
+        }
+      }
+      qtdeToSplit[i] = qtde;
+    }
+
+    for (let index = 1; index < qtdeToSplit.length; index++) {
+      const qtde = qtdeToSplit[index];
+      for (const drinkerCalculating of drinkers) {
+        const end = this.fim(drinkerCalculating, beerAmmount);
+        if (index >= drinkerCalculating.start_in && index <= end) {
+          drinkerCalculating.ammout_drinked += 1 / qtde;
+        }
+      }
+    }
+
+    for (const drinkerCalculating of drinkers) {
+      drinkerCalculating.should_pay = (drinkerCalculating.ammout_drinked * this.beer_unit_value) * (this.includeTip ? 1.1 : 1);
+    }
+  }
+
+  fim(d1: Drinker, beerAmmount: number) {
+    return d1.end_in ? d1.end_in : beerAmmount;
+  }
+
 }
